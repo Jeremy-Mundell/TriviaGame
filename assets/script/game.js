50945efbc6a38fd37940ab133e121394a28cb7f3
Jeremy Mundell <jeremiahmundell@gmail.com>
2017-08-22 15:52:54 -0400

    new changes

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,1 +1,218 @@
-// alert("connected");
+
+
+var panel = $('#quiz-area'); 
+$(document).on('click', '#start', function(event) {
+    game.startGame();
+});
+
+
+var questionsArray = [{
+
+  question: "What is the name of Best-Selling author of Game of Thrones?",
+  answers: ["Stephen King", "George R.R. Martin", "George Orwell", "George Elliot"],
+  
+  correctAnswer: 1
+}, {
+
+  question: "What is the name of the Onion Knight?",
+  answers: ["Jamie Lannister", "Davos Seaworth", "Euron Greyjoy", "Podrick Payne"],
+  
+  correctAnswer: 1
+}, {
+
+  question: "Who is the current King in the North?",
+  answers: ["Tyrion Lannister", "Sandor Clegane", "Walter Frey", "Jon Snow"],
+  
+  correctAnswer: 3
+}, {
+
+  question: "What noble house always pays their debts? ",
+  answers: ["Lannister", "Tyrell", "Stark", "Frey"],
+  
+  correctAnswer: 0
+}, {
+
+  question: "What noble house rode dragons to war? ",
+  answers: ["Stark", "Lannister", "Targaryen", "Bolton"],
+  
+  correctAnswer: 3
+}, {
+
+  question: "What are the undead creatures beyond the northern wall called?",
+  answers: ["Zombies", "White-walkers", "Vampires", "White-talkers"],
+  
+  correctAnswer: 1
+}, {
+
+  question: "What sharp material is discovered to destroy the undead creatures of the north?",
+  answers: ["Dragon glass", "Glass", "Stained glass", "Swords"],
+  
+  correctAnswer: 0
+},
+
+  {
+
+  question: "Who was married to Tyrion Lannister as a joke?",
+  answers: ["Arya Stark", "Cersei Lannister", "Breinne Tarth", "Sansa Stark"],
+  
+  correctAnswer: 3 
+},
+
+  {
+
+  question: "How did Viserys Targaryen die?",
+  answers: ["Natural causes", "Sword fight", "Drowning in molten gold", "Dragon fire"],
+  
+  correctAnswer: 2
+
+},
+
+  {
+
+  question: "What is the name of the 3rd book in the series of a Song of Fire and Ice?",
+  answers: ["A Game of Thrones", "A Storm of Swords", "A Feast for Crows", "The Shining"],
+  
+  correctAnswer: 1
+
+}]
+
+
+
+	var game = {
+
+    // Game Variables
+
+    questions: questionsArray,  
+    questionNum: 0,
+    correct: 0,
+   	wrong: 0,
+    counter:60,
+
+  	// Timer function. Counts down
+
+  	countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      alert('TIME IS UP!');
+      game.done();
+    }
+  },
+
+  	// after pressing start. Game page appends with questions and timer starts
+    startGame: function() {
+    	timer = setInterval(game.countdown, 1000);
+
+    	$('#sub-wrap').prepend('<h2>Remaining Time: <span id="counter-number">60</span> Seconds</h2>');
+    	$('#start').remove();
+    	$('#question_div').append("<h1>GO!</h1>");
+    	for (var i = 0; i < questionsArray.length; i++) {
+    		$('#question_div').append('<h2>' + questionsArray[i].question + '</h2>');
+    		for (var t = 0; t < 4; t++) {
+    			$('#question_div').append('<input type="radio" name="answers'+i+'">' + questionsArray[i].answers[t] );
+    		}
+		}
+
+	},
+
+
+
+
+
+
+	done: function() {
+
+// Answer check formula...repeated for each question to check for right & wrong answers.
+
+    // $.each($("input[name='question-0']:checked"), function() {
+    //   if ($(this).val() == questionsArray[0].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-1']:checked"), function() {
+    //     if ($(this).val() == questionsArray[1].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-2']:checked"), function() {
+    //   if ($(this).val() == questionsArray[2].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-3']:checked"), function() {
+    //   if ($(this).val() == questionsArray[3].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-4']:checked"), function() {
+    //   if ($(this).val() == questionsArray[4].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-5']:checked"), function() {
+    //   if ($(this).val() == questionsArray[5].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-6']:checked"), function() {
+    //   if ($(this).val() == questionsArray[6].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+    // });
+    // $.each($("input[name='question-7']:checked"), function() {
+    //   if ($(this).val() == questionsArray[7].correctAnswer) {
+    //     game.correct++;
+    //   } else {
+    //     game.wrong++;
+    //   }
+
+    //   console.log(done);
+    // });
+
+// ISSUE - Cannot get the DONE button to work and appear at the bottom of the page to activate the results function.
+    panel.append('<button id="done">Done</button>');
+
+    // this.result();
+  },
+
+  // RESULTS function to display right and wrong answers.
+
+    result: function() {
+
+    clearInterval(timer);
+
+    $('#subwrapper h2').remove();
+    panel.html('<h2>All Done!</h2>');
+    panel.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+    panel.append('<h3>Wrong Answers: ' + this.wrong + '</h3>');
+  }
+
+};
+
+//functions
+
+
+
+	// var newDiv = document.createElement("div")
+	// //reference the tag 
+	// newDiv.className = "addedClass"
+	// var textHere = document.createTextNode("Write text here...")
+	// newDiv.appendChild(textHere)
+	// var addHere = document.getElementById("addHere")
+	// // on this header id add a new div
+	// addHere.appendChild(newDiv)
